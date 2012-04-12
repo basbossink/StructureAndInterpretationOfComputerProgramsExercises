bdd27a4b49c250170e9db0b5d1c4ccd0dbd65616
Bas Bossink <bas.bossink@gmail.com>
2012-04-13 00:46:59 +0200

    Added license information.
    Added the license and licensing information to each file.

--- ch01/1.01.scm
+++ ch01/1.01.scm
@@ -1,12 +1,33 @@
+;; Copyright 2012 Bas Bossink 
+;; <bas.bossink@gmail.com>
+;; http://basbossink.github.com/
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 (require-extension test)
+
 (test-begin "1.1")
+
 (test 10 10)
 (test 12 (+ 5 3 4))
 (test 8 (- 9 1))
 (test 3 (/ 6 2))
 (test 6 (+ (* 2 4) (- 4 6)))
+
 (define a 3)
 (define b (+ a 1))
+
 (test 19 (+ a b (* a b)))
 (test #f (= a b))
 (test 3 (if (and (> a b) (< b (* a b)))
@@ -20,4 +41,5 @@
                ((< a b) b)
                (else -1))
             (+ a 1)))
+
 (test-end)
--- ch01/1.03.scm
+++ ch01/1.03.scm
@@ -1,13 +1,34 @@
+;; Copyright 2012 Bas Bossink 
+;; <bas.bossink@gmail.com>
+;; http://basbossink.github.com/
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 (require-extension test)
+
 (define (sol x y z) 
   (define (sum-of-squares i j)
     (+ (* i i) (* j j)))
   (cond ((and (<= x y) (<= x z)) (sum-of-squares y z))
         ((and (<= y x) (<= y z)) (sum-of-squares x z))
         ((and (<= z x) (<= z y)) (sum-of-squares x y))))
+
 (test-begin "1.3")
+
 (test 13 (sol 1 2 3))
 (test 4 (sol 0 2 0))
 (test 1 (sol (- 1) 1 0))
 (test 20 (sol 2 4 (- 37)))
+
 (test-end)
