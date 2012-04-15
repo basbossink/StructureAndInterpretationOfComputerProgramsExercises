bf508121f547fc564eb95a09255b49d2c7c6d429
Bas Bossink <bas.bossink@gmail.com>
2012-04-15 22:29:14 +0200

    Finish 1.7.

--- ch01/1.07.scm
+++ ch01/1.07.scm
@@ -18,10 +18,13 @@
 (require-extension test)
 
 (define (sqrt-iter guess x)
-  (if (good-enough? guess x)
-    guess
-    (sqrt-iter (improve guess x)
-               x)))
+  (define (sqrt-iter-measure-progress guess previous-guess x)
+    (if (good-enough? guess previous-guess)
+      guess
+      (sqrt-iter-measure-progress (improve guess x)
+                                guess
+                                x)))
+  (sqrt-iter-measure-progress guess x x))
 
 (define (improve guess x)
   (average guess (/ x guess)))
@@ -29,14 +32,15 @@
 (define (average x y)
   (/ (+ x y) 2))
 
-(define (good-enough? guess x)
-  (< (abs (/ (- (* guess guess) x) x)) 0.00001))
+(define (good-enough? guess previous-guess)
+  (< (abs (- 1 (/ previous-guess guess))) 1.e-5))
 
 (test-begin "1.7")
 
 (test 2.0 (sqrt-iter 1 4))
 (test 3.0 (sqrt-iter 1 9))
 (test 5.0 (sqrt-iter 1 25))
+(test 1.e-40 (sqrt-iter 1 1.e-80))
 (test 1.e-4 (sqrt-iter 1 1.e-8))
 (test 1e40 (sqrt-iter 1 1e80))
 
