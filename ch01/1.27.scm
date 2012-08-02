eb6d5d368fef4d6df66d209726d19c93c8bccbdd
Bas Bossink <bas.bossink@gmail.com>
2012-08-02 21:53:43 +0200

    Use faster verion of expmod.

--- ch01/1.27.scm
+++ ch01/1.27.scm
@@ -17,11 +17,12 @@
 
 (require-extension test)
 
+(define (square x) (* x x))
+
 (define (expmod base exp m)
   (cond ((= exp 0) 1)
         ((even? exp)
-         (remainder (* (expmod base (/ exp 2) m)
-                       (expmod base (/ exp 2) m))
+         (remainder (square (expmod base (/ exp 2) m))
                     m))
         (else
          (remainder (* base (expmod base (- exp 1) m))
